691eabcb216c8ab39f2baf6bb510af72dfd01983
CapybaraYT1 <Capybarayt2.official@gmail.com>
2026-05-09 09:48:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
-BOT_TOKEN = "8111442064:AAGSdtAnKMUA-2VAlYo0XEfKvyTmg5ei4CM"
-DATABASE_URL = "postgresql://postgres:Furry_2027!@db.zbudmrrslcnjvhfmehxz.supabase.co:5432/postgres"
+import os
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+DATABASE_URL = os.getenv("DATABASE_URL")
